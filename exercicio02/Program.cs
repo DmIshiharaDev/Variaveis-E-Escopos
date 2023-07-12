7da97c959442d6b1e8bde3b31e63f6464ee6ee6a
DmIshiharaDev <139144547+DmIshiharaDev@users.noreply.github.com>
2023-07-12 14:05:05 -0300

    Push dos arquivos feitos sobre Variáveis e Escopos

---
+++ exercicio02/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // toda variável tem um tipo e identificador
+
+            // tipo, identificador:
+
+            // Declaração de variáveis:
+
+            char primeiraLetraNome;
+            string nome;
+            float valor;
+            double valorAlto;
+  
+
+            // Atribuindo valores as variáveis
+
+            primeiraLetraNome = 'D';
+            nome = "Daniel";
+            valor = 0.4f;
+            valorAlto = 1231.45;
+
+            Console.WriteLine(primeiraLetraNome);
+            Console.WriteLine(nome);
+            Console.WriteLine(valor);
+            Console.WriteLine(valorAlto);
+
+        }
+    }
+}
